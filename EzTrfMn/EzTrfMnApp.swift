9476f92d6e67a066c07b1defa3598c29b272c708
ssh <ssoohyun126@gmail.com>
2023-02-17 22:58:26 +0900

    Initial Commit

---
+++ EzTrfMn/EzTrfMnApp.swift
@@ -0,0 +1,17 @@
+//
+//  EzTrfMnApp.swift
+//  EzTrfMn
+//
+//  Created by 손수현 on 2023/02/17.
+//
+
+import SwiftUI
+
+@main
+struct EzTrfMnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
