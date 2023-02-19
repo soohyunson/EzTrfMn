f73267bc3c7511ee6685b48cb4f808bbe96d62d9
ssh <ssoohyun126@gmail.com>
2023-02-19 14:42:50 +0900

    로그인1일차

---
+++ EzTrfMn/LoginView.swift
@@ -0,0 +1,23 @@
+//
+//  LoginView.swift
+//  EzTrfMn
+//
+//  Created by 손수현 on 2023/02/19.
+//
+
+import SwiftUI
+
+
+struct LoginView: View {
+    var body: some View {
+        Text("로그인")
+        
+        
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ EzTrfMn/LoginViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  EzTrfMn
+//
+//  Created by 손수현 on 2023/02/19.
+//
+
+import SwiftUI
+
+struct LoginViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginViewModel()
+    }
+}
