7ab5e30f45a77164f2617cd331f827add9d188cd
ssh <ssoohyun126@gmail.com>
2023-06-24 16:24:47 +0900

    view add

---
+++ EzTrfMn/TransferView.swift
@@ -0,0 +1,20 @@
+//
+//  TransferView.swift
+//  EzTrfMn
+//
+//  Created by 손수현 on 2023/06/24.
+//
+
+import SwiftUI
+
+struct TransferView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransferView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransferView()
+    }
+}
